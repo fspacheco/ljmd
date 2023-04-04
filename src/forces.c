341f79b44ba760cec110e45ddebcc0e91b5c840d
Fernando S. Pacheco <fspacheco@gmail.com>
2023-04-04 15:54:56 +0200

    Parallel region

--- src/forces.c
+++ src/forces.c
@@ -20,7 +20,7 @@ void force(mdsys_t *sys)
     double rx,ry,rz;
     int i,j;
     //new variables for the optimization
-    double c12,c6,rcsq;
+    double c12,c6,rcsq, rsq, rm6, rm2;
     /* zero energy and forces */
     sys->epot=0.0;
     azzero(sys->fx,sys->natoms);
@@ -32,30 +32,37 @@ void force(mdsys_t *sys)
     c6 =4.0*sys->epsilon*pow(sys->sigma, 6.0); //c6 is the 6th power of sigma
     rcsq=sys->rcut*sys->rcut; //square of the cutoff radius
 
-	double epot_tmp = 0; // gcc does not make reduction directly on sys->epot
-#ifdef _OPENMP
-#pragma omp parallel for default(shared) private(i,j,rx,ry,rz,ffac) reduction(+:epot_tmp)
+	double epot_tmp = 0.0; // gcc does not make reduction directly on sys->epot
+#if defined(_OPENMP)
+#pragma omp parallel default(shared) private(i,j,rx,ry,rz,ffac) 
+	{
+	double *omp_forces;
+	omp_forces = (double*)malloc(3*sys->natoms*sizeof(double));
+	if (omp_forces == NULL) {
+		fprintf(stderr,"Error allocating memory for omp_forces\n");
+		exit(1);
+	}
+	azzero(omp_forces, 3*sys->natoms);
+#pragma omp for reduction(+:epot_tmp) nowait
 #endif
     for(i=0; i < (sys->natoms); ++i) {
         for(j=i+1; j < (sys->natoms); ++j) { // The original code was j=0, which means that the force on particle i was computed twice. This is fixed by starting the loop at j=i+1
 
            // particles have no interactions with themselves
            // if (i==j) continue; //it will be useless, since j starts from i+1
+
             // get distance between particle i and j
             rx=pbc(sys->rx[i] - sys->rx[j], 0.5*sys->box);
             ry=pbc(sys->ry[i] - sys->ry[j], 0.5*sys->box);
             rz=pbc(sys->rz[i] - sys->rz[j], 0.5*sys->box);
             double rsq = rx*rx + ry*ry + rz*rz;  //remove the expensive sqrt() function from the inner loop
-			//if (i==0 && j==8) {
-			//	fprintf(stderr, "DBG: inside i %d j %d: rsq = %f < %f?\n", i, j, rsq, rcsq);
-			//}
+
             // compute force and energy if within cutoff
            if (rsq < rcsq) {
                 //remove the expensive pow() and division functions from the inner loop
                 double rm6,rm2; rm2=1.0/rsq; rm6=rm2*rm2*rm2;
                 ffac = (12.0*c12*rm6 - 6.0*c6)*rm6*rm2;
                 epot_tmp += rm6*(c12*rm6 - c6); //here it is not necessary to multiply by 0.5, since the force is computed once for each pair of particles
-				//fprintf(stderr, "DBG: inside i %d j %d: epot_tmp = %f\n", i, j, epot_tmp);
 
                 //ffac = -4.0*sys->epsilon*(-12.0*pow(sys->sigma/r,12.0)/r
                 //                         +6*pow(sys->sigma/r,6.0)/r);
@@ -63,10 +70,8 @@ void force(mdsys_t *sys)
                 //sys->epot += 0.5*4.0*sys->epsilon*(pow(sys->sigma/r,12.0)
                 //                               -pow(sys->sigma/r,6.0)); //here it is necessary to multiply by 0.5, since the force is computed twice for each pair of particles
                 
-#if defined(_OPENMP)
-#pragma omp critical
-#endif
-				{
+#if !defined(_OPENMP)
+				//fprintf(stderr, "DBG: should NOT be here with OpenMP\n");
                 sys->fx[i] += rx*ffac; // remove division based on previous changes
                 sys->fy[i] += ry*ffac;
                 sys->fz[i] += rz*ffac;
@@ -74,9 +79,26 @@ void force(mdsys_t *sys)
                 sys->fx[j] -= rx*ffac;  // remove division ...
                 sys->fy[j] -= ry*ffac;
                 sys->fz[j] -= rz*ffac;
-				}
-            } // end if
+#else
+				omp_forces[3*i] += rx*ffac;
+				omp_forces[3*i+1] += ry*ffac;
+				omp_forces[3*i+2] += rz*ffac;
+				omp_forces[3*j] -= rx*ffac;
+				omp_forces[3*j+1] -= ry*ffac;
+				omp_forces[3*j+2] -= rz*ffac;
+#endif
+            }
         } // end for j
     } // end for i
+#if defined(_OPENMP)
+#pragma omp critical
+	for (i=0; i<sys->natoms; i++) {
+		sys->fx[i] += omp_forces[3*i];
+		sys->fy[i] += omp_forces[3*i+1];
+		sys->fz[i] += omp_forces[3*i+2];
+	}
+	free(omp_forces);
+	}
+#endif
 	sys->epot += epot_tmp;
 }
