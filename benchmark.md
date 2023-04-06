5d07713801d036a6a72177f94db7dff7f945c84a
Fernando S. Pacheco <fspacheco@gmail.com>
2023-04-06 17:12:14 +0200

    Timing on Ulysses

--- benchmark.md
+++ benchmark.md
@@ -1,30 +1,100 @@
-commit 30b53a3780cfe6a6be16611968a4210523fa3b5a
-argon_2916.inp in my notebook: i5 2 cores with -g -O0
+# Results
+
+## Initial tests on my notebook
+
+### commit 30b53a3780cfe6a6be16611968a4210523fa3b5a (reduction/critical)
+
+argon_2916.inp in my notebook: i5 2 cores with -g **-O0**
+
+```
 Starting simulation with 2916 atoms for 1000 steps.
 Serial Simulation Done. Run time:    129.456s
 1 thread Simulation Done. Run time:    133.827s
 2 threads Simulation Done. Run time:    104.084s
 4 threads Simulation Done. Run time:     75.539s
 8 threads Simulation Done. Run time:     61.835s
+```
 
--O3
+### Same commit compiled with **-O3**
+
+```
 1 thread Simulation Done. Run time:     47.017s
 2 threads Simulation Done. Run time:     38.781s
 4 threads Simulation Done. Run time:     32.115s
 8 threads Simulation Done. Run time:     25.753s
+```
 
--O3 -ffast-math -fomit-frame-pointer
+### Same commit compiled with **-O3 -ffast-math -fomit-frame-pointer**
+
+```
 1 thread Simulation Done. Run time:     54.320s
 4 threads Simulation Done. Run time:     31.142s
 8 threads Simulation Done. Run time:     27.023s
+```
 
+### commit 341f79b44ba760cec110e45ddebcc0e91b5c840d (parallel region) compiled with **-O3**
 
-commit 341f79b44ba760cec110e45ddebcc0e91b5c840d
--O3
-Parallel region alla Bussi
+```
 1 thread Simulation Done. Run time:     45.972s
 2 threads Simulation Done. Run time:     36.478s
 4 threads Simulation Done. Run time:     27.247s
 8 threads Simulation Done. Run time:     20.165s
+```
 
-1000          60.24508534         523.47323926       -4425.11992013       -3901.64668087
+## Running on Ulysses
+
+At `/home/fsantana/ljmd`
+
+## Steps to compile
+
+```
+module load cmake
+module load intel/2021.1
+cmake -D CMAKE_C_COMPILER=gcc ..
+make -j2 VERBOSE=1
+```
+
+### commit e35a0b05b2ed2324aaf9fbf2615c28f6fc9512ae (Parallel region) (with -O3)
+
+File omp_md.o9147871
+
+Allocated 10 cpus
+
+| Num threads     | Time (s) 108atoms | 2916atoms |
+| --------------- | ----------------- | --------- |
+|20               | 0.129             | 9.595
+|10               | 0.062             | 11.936
+|5                | 0.083             | 18.094
+|2                | 0.155             | 32.965
+|1                | 0.194             | 42.235
+
+### commit c3cba54507dec408ff548cd6ff2f2235a935b164 (OpenMP version compiled with -O3) (reduction/critical)
+
+File omp_md.o9147876
+
+Allocated 10 cpus
+
+| Num threads     | Time (s) 108atoms | 2916atoms |
+| --------------- | ----------------- | --------- |
+| 20              | 0.879             | 61.665    |
+| 10              | 1.399             | 50.857    |
+| 5               | 0.744             | 36.887    |
+| 2               | 0.382             | 41.398    |
+| 1               | 0.174             | 50.889    |
+
+### The serial version from the same commit, but compiled with -O3
+
+File omp_md.o9147913
+
+| Num threads     | Time (s) 108atoms | 2916atoms |
+| --------------- | ----------------- | --------- |
+| 1               | 0.137             | 43.643
+
+### The serial version from the same commit, but compiled with -O0
+
+File omp_md.o9147922
+
+| Num threads     | Time (s) 108atoms | 2916atoms |
+| --------------- | ----------------- | --------- |
+| 1               | 0.331             | 118.622   |
+
---
+++ test/ulysses/send_job_versionserial.sh
@@ -0,0 +1,77 @@
+#!/usr/bin/env bash
+
+
+#SBATCH --job-name=omp_md      		# The name of your job, you'll se it in squeue.
+#SBATCH --mail-type=NONE              	# Mail events (NONE, BEGIN, END, FAIL, ALL). Sends you an email when the job begins, ends, or fails; you can combine options.
+
+#SBATCH --ntasks=1                   # Number of MPI ranks (1 for MPI serial job)
+#SBATCH --cpus-per-task=10           # Number of threads per MPI rank (MAX: 2x32 cores on _partition_2, 2x20 cores on _partition_1) 
+#SBATCH --nodes=1                    # Number of nodes
+#SBATCH --ntasks-per-node=1          # How many tasks on each node
+#SBATCH --ntasks-per-core=1          # How many tasks on each core (set to 1 to be sure that different tasks run on different cores on multi-threaded systems)
+
+#SBATCH --mem=900mb                 # Memory per node (MAX: 63500 on the new ones, 40000 on the old ones); incompatible with --mem-per-cpu.
+
+#SBATCH --partition=regular1         # Partition (queue). Avail: regular1, regular2, long1, long2, wide1, wide2, gpu1, gpu2. Multiple partitions are possible.
+#SBATCH --time=00:05:00              # Time limit hrs:min:sec
+#SBATCH --output=%x.o%j              # Standard output log in TORQUE-style -- WARNING: %x requires a new enough SLURM. Use %j for regular jobs and %A-%a for array jobs
+#SBATCH --error=%x.e%j               # Standard error  log in TORQUE-style -- WARNING: %x requires a new enough SLURM. Use %j for regular jobs and %A-%a for array jobs
+
+# ==== Modules part (load all the modules) ===== #
+
+	module load cmake/3.15.4
+	module load intel/2021.1
+
+# ==== End of Modules part (load all the modules) ===== #
+
+
+
+# ==== Info part (say things) ===== #
+#
+NOW=`date +%H:%M-%a-%d/%b/%Y`
+echo '------------------------------------------------------'
+echo 'This job is allocated on '$SLURM_JOB_CPUS_PER_NODE' cpu(s)'
+echo 'Job is running on node(s): '
+echo  $SLURM_JOB_NODELIST
+echo '------------------------------------------------------'
+echo 'WORKINFO:'
+echo 'SLURM: job starting at           '$NOW
+echo 'SLURM: sbatch is running on      '$SLURM_SUBMIT_HOST
+echo 'SLURM: executing on cluster      '$SLURM_CLUSTER_NAME
+echo 'SLURM: executing on partition    '$SLURM_JOB_PARTITION
+echo 'SLURM: working directory is      '$SLURM_SUBMIT_DIR
+echo 'SLURM: current home directory is '$(getent passwd $SLURM_JOB_ACCOUNT | cut -d: -f6)
+echo ""
+echo 'JOBINFO:'
+echo 'SLURM: job identifier is         '$SLURM_JOBID
+echo 'SLURM: job name is               '$SLURM_JOB_NAME
+echo ""
+echo 'NODEINFO:'
+echo 'SLURM: number of nodes is        '$SLURM_JOB_NUM_NODES
+echo 'SLURM: number of cpus/node is    '$SLURM_JOB_CPUS_PER_NODE
+echo 'SLURM: number of gpus/node is    '$SLURM_GPUS_PER_NODE
+echo '------------------------------------------------------'
+#
+# ==== End of Info part (say things) ===== #
+
+
+# # Should not be necessary anymore with SLURM, as this is the default, but you never know...
+# cd $SLURM_SUBMIT_DIR
+
+
+# ==== JOB COMMANDS ===== #
+
+cd build/examples
+../ljmd-serial.x  < argon_108.inp 
+../ljmd-serial.x  < argon_108.inp 
+
+../ljmd-serial.x  < argon_2916.inp  
+../ljmd-serial.x  < argon_2916.inp  
+
+# ==== END OF JOB COMMANDS ===== #
+
+
+
+# Wait for processes, if any.
+echo "Waiting for all the processes to finish..."
+wait
